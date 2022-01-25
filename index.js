a9691bee5700490ea2a38008c6eebe7063c5b9cc
Danial Aftab <danial@ziwo.io>
2022-01-25 15:00:44 +0400

    Change to post

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/notify', (req, res) => {
+app.post('/notify', (req, res) => {
     console.log("Request", req)
     res.send('Hello World!')
   })

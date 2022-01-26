263fb4c1471d89dfb459aeb2b9a90e5403672828
Danial Aftab <danial@ziwo.io>
2022-01-26 13:50:37 +0400

    update token logs

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  console.log("Request Token:", req.query.token)
+  res.send('Sign up for your ziwo account!')
 })
 
 app.post('/notify', (req, res) => {
-    console.log("Request Token:", req.query.token)
     res.send('Sign up for your ziwo account!')
   })
 

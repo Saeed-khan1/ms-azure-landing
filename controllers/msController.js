4f9237de7eaa0ceb4659bd4d489830cf2884101a
Danial Aftab <danial@ziwo.io>
2022-01-31 15:28:36 +0400

    refactor

---
+++ controllers/msController.js
@@ -0,0 +1,69 @@
+const msal = require('@azure/msal-node');
+const port = process.env.PORT || 3000
+const config = {
+    auth: {
+        clientId: "7326c157-6b14-4bd9-8a76-856d27861bbe",
+        authority: "https://login.microsoftonline.com/common",
+        clientSecret: "L6n7Q~wRphjscUskU9_3GmKquI0w8TQsIi3zG"
+    },
+    system: {
+        loggerOptions: {
+            loggerCallback(loglevel, message, containsPii) {
+                console.log(message);
+            },
+            piiLoggingEnabled: false,
+            logLevel: msal.LogLevel.Verbose,
+        }
+    }
+};
+
+class MsController {
+
+    cca = null
+    constructor() {
+        this.cca = new msal.ConfidentialClientApplication(config);
+    }
+
+    adLogin(req, res) {
+        console.log("Request Token:", req.query.token)
+
+        const authCodeUrlParameters = {
+            scopes: ["user.read"],
+            redirectUri: `http://localhost:${port}/redirect`,
+        };
+
+        // get url to sign user in and consent to scopes needed for application
+        this.cca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+            res.redirect(response);
+        }).catch((error) => console.log(JSON.stringify(error)));
+    }
+
+
+    callback(req, res) {
+        const tokenRequest = {
+            code: req.query.code,
+            scopes: ["user.read"],
+            redirectUri: `http://localhost:${port}/redirect`,
+        };
+
+        this.cca.acquireTokenByCode(tokenRequest).then((response) => {
+            console.log("\nResponse: \n:", response);
+            res.sendFile(__dirname + '/views/complete.html');
+        }).catch((error) => {
+            console.log(error);
+            res.status(500).send(error);
+        });
+    }
+
+
+    landing(req, res) {
+        console.log("Request Token:", req.query.token)
+        res.sendFile(__dirname + '/views/landing.html')
+    }
+
+    notify(req, res) {
+        res.send('Sign up for your ziwo account!')
+    }
+}
+
+module.exports = MsController

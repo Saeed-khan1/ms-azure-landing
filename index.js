9c6baf3dde1e12e4fbee532553ac9d2066a6c9db
saeed-khan1 <saeed.abu@ziwo.io>
2022-02-14 23:06:35 +0400

    landing page design

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require('express')
-const { use } = require('express/lib/application')
-const routes = require('./routes')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const path = require("path");
+const { use } = require("express/lib/application");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+const app = express();
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json())
-const port = process.env.PORT || 3000
-require('dotenv').config()
+app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
+require("dotenv").config();
 
-
-routes(app)
-app.use(express.static('./views'))
-
+routes(app);
+app.use(express.static(path.join(__dirname, "views")));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
